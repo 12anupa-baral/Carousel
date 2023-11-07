ec3b0425ddac4cab32ca05811acf8bb883c45fd2
Anupa Baral <baralanupa988@gmail.com>
2023-11-07 22:07:01 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+// let slideIndex = 0;
+// const slides = document.getElementsByClassName("carousel-slide");
+
+// function showSlides() {
+//   for (let i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slides[slideIndex].style.display = "block";
+// }
+
+// document.querySelector(".prev-button").addEventListener("click", () => {
+//   slideIndex -= 1;
+//   if (slideIndex < 0) {
+//     slideIndex = slides.length - 1;
+//   }
+//   showSlides();
+// });
+
+// document.querySelector(".next-button").addEventListener("click", () => {
+//   slideIndex += 1;
+//   if (slideIndex >= slides.length) {
+//     slideIndex = 0;
+//   }
+//   showSlides();
+// });
+
+// showSlides();
+
+let slideIndex = 0;
+const slides = document.getElementsByClassName("carousel-slide");
+
+function showSlides() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex].style.display = "block";
+}
+
+document.querySelector(".prev-button").addEventListener("click", () => {
+  slideIndex -= 1;
+  if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+  }
+  showSlides();
+});
+
+document.querySelector(".next-button").addEventListener("click", () => {
+  slideIndex += 1;
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  }
+  showSlides();
+});
+
+// Initial display
+showSlides();
