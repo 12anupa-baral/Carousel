feb0cdad96056ef8ddd05f71637a79485a074267
Anupa Baral <baralanupa988@gmail.com>
2023-11-10 08:07:48 +0545

    changed js

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-// let slideIndex = 0;
-// const slides = document.getElementsByClassName("carousel-slide");
-
-// function showSlides() {
-//   for (let i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   slides[slideIndex].style.display = "block";
-// }
-
-// document.querySelector(".prev-button").addEventListener("click", () => {
-//   slideIndex -= 1;
-//   if (slideIndex < 0) {
-//     slideIndex = slides.length - 1;
-//   }
-//   showSlides();
-// });
-
-// document.querySelector(".next-button").addEventListener("click", () => {
-//   slideIndex += 1;
-//   if (slideIndex >= slides.length) {
-//     slideIndex = 0;
-//   }
-//   showSlides();
-// });
-
-// showSlides();
-
 let slideIndex = 0;
 const slides = document.getElementsByClassName("carousel-slide");
 
@@ -52,5 +24,4 @@ document.querySelector(".next-button").addEventListener("click", () => {
   showSlides();
 });
 
-// Initial display
 showSlides();
